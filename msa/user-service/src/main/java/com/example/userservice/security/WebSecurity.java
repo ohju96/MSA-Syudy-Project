0d5ee4c48dbb60c70ca29ecdd03c6c0a02fdd153
ohju96 <ohju96@naver.com>
2022-07-18 08:30:37 +0900

    user service : Spring Security 사용 및 비밀번호 암호화

---
+++ msa/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -0,0 +1,20 @@
+package com.example.userservice.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/users/**").permitAll();
+
+        // h2-console에 접근할 수 있게 설정한다.
+        http.headers().frameOptions().disable();
+    }
+}
