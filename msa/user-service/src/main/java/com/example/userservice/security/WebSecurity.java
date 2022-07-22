afed08351e209bbad253f6473eba2ce5746dad4c
ohju96 <ohju96@naver.com>
2022-07-22 21:57:24 +0900

    users service : AuthenticationFilter

--- msa/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ msa/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -5,6 +5,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.servlet.Filter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -12,9 +14,20 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+//        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+                .hasAnyAuthority("192.168.56.1")
+                        .and()
+                        .addFilter(getAuthentionFilter());
 
         // h2-console에 접근할 수 있게 설정한다.
         http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthentionFilter() throws Exception{
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
     }
 }
